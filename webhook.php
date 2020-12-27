d06a031adb587379b104d149257d5c54cf545b90
kevocam.seo@gmail.com <kevocam.seo@gmail.com>
2020-12-27 00:20:10 -0500

    webhoks

---
+++ webhook.php
@@ -0,0 +1,20 @@
+<?php
+
+    MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+
+    switch($_POST["type"]) {
+        case "payment":
+            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+            break;
+        case "plan":
+            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            break;
+        case "subscription":
+            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            break;
+        case "invoice":
+            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            break;
+    }
+
+?>
