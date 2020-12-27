d973d53867c0250e72868b1c5d300448ea0b73ac
kevocam.seo@gmail.com <kevocam.seo@gmail.com>
2020-12-26 22:02:34 -0500

    add checkout

--- detail.php
+++ detail.php
@@ -1,22 +1,72 @@
 <?php
-                                        // SDK de Mercado Pago
-                                        require __DIR__ .  '/vendor/autoload.php';
+    // SDK de Mercado Pago
+    require __DIR__ .  '/vendor/autoload.php';
 
-                                        // Agrega credenciales
-                                        MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
+    // Agrega credenciales
+    MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
 
-                                        // Crea un objeto de preferencia
-                                        $preference = new MercadoPago\Preference();
+    // Crea un objeto de preferencia
+    $preference = new MercadoPago\Preference();
 
-                                        // Crea un ítem en la preferencia
-                                        
-                                        // Crea un ítem en la preferencia
-                                        $item = new MercadoPago\Item();
-                                        $item->title = $_POST['title'];
-                                        $item->quantity = $_POST['unit'];
-                                        $item->unit_price = $_POST['price'];
-                                        $preference->items = array($item);
-                                        $preference->save();
+    // Crea un ítem en la preferencia
+    
+    // Crea un ítem en la preferencia
+    $item = new MercadoPago\Item();
+    $item->id = "1234";
+    $item->category_id = "phones";
+    $item->title = $_POST['title'];
+    $item->description = "Dispositivo móvil de Tienda e-commerce";    
+    $item->imagen = $_POST['img'];
+    $item->quantity = $_POST['unit'];
+    $item->unit_price = $_POST['price'];
+    $item->external_reference = "kevocam.seo@gmail.com";
+    
+
+
+    $preference->items = array($item);
+
+    $preference->back_urls = array(
+        "success" => "https://kevocam-mp-commerce-php.herokuapp.com/success.php",
+        "failure" => "https://kevocam-mp-commerce-php.herokuapp.com/failure.php",
+        "pending" => "https://kevocam-mp-commerce-php.herokuapp.com/pending"
+    );
+    $preference->auto_return = "approved";
+
+    $preference->payment_methods = array(
+        "excluded_payment_methods" => array(
+          array("id" => "pagoefectivo_atm")
+        ),
+        "excluded_payment_types" => array(
+          array("id" => "diners") 
+        ),
+        "installments" => 6
+      );
+
+    $preference->save();
+
+
+    
+    //Comprador info
+    $payer = new MercadoPago\Payer();
+    $payer->name = "Lalo";
+    $payer->surname = "Landa";
+    $payer->email = "test_user_46542185@testuser.com";
+    $payer->phone = array(
+        "area_code" => "52",
+        "number" => "5549737300"
+    );
+    
+    $payer->identification = array(
+        "type" => "DNI",
+        "number" => "22334445"
+    );
+    
+    $payer->address = array(
+        "street_name" => "Insurgentes Sur",
+        "street_number" => 1602,
+        "zip_code" => "03940"
+    );
+    
                                     ?>
 
 
