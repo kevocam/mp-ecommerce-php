70cfc499bd808e2da775201ac2684a119ef28f45
kevocam.seo@gmail.com <kevocam.seo@gmail.com>
2020-12-26 22:28:37 -0500

    fixs

--- success.php
+++ success.php
@@ -85,9 +85,9 @@
             <div class="main">   
 
             <!-- https://kevocam-mp-commerce-php.herokuapp.com/success.php?collection_id=12815839719&collection_status=approved&payment_id=12815839719&status=approved&external_reference=null&payment_type=credit_card&merchant_order_id=2148794700&preference_id=677408439-b1a6ecbc-c69d-4efb-b08e-05f93bfcf685&site_id=MPE&processing_mode=aggregator&merchant_account_id=null -->         
-    Transacción satisfactoria
-            <p>Orden: <?php echo $_POST[collection_id] ?></p>
-            <p>Tipo: <?php echo $_POST[payment_type] ?></p>
+    Transacción satisfactoria 
+            <p>Orden: <?php echo $_GET['collection_id'] ?></p>
+            <p>Tipo: <?php echo $_POST['payment_type'] ?></p>
 
                 <img src="./assets/success.png" alt="">
                 <div class="main__back">
