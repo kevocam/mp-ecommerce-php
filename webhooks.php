ecc96148e46f26d2f6678d161fc78f61c5eb01de
kevocam.seo@gmail.com <kevocam.seo@gmail.com>
2020-12-28 13:17:23 -0500

    webhookds

--- webhooks.php
+++ webhooks.php
@@ -1,9 +1,59 @@
+
 <?php
-$json_event = file_get_contents('php://input', true);
 
-file_put_contents('php://stderr', $json_event);
-file_put_contents(__DIR__.'/log/webhook.log', $json_event);
+require __DIR__ .  '/vendor/autoload.php';
 
-$data = json_encode($_POST);
-file_put_contents('php://stderr', $data);
-file_put_contents(__DIR__.'/log/log.log', $data);
+MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
+
+$type = isset($_GET["type"]) ? $_GET["type"] : isset($_POST["type"]) ? $_POST["type"] : null;
+$id = isset($_GET["id"]) ? $_GET["id"] : isset($_POST["id"]) ? $_POST["id"] : null;
+
+$datos = [
+    'type' => $_GET['type'],
+    'topic' => $_GET['topic'],
+    'id' => $_GET['id'],
+];
+
+if($_GET['topic']=="payment" || $_GET['topic'=="merchant_order"]){
+    file_put_contents('webhook.log', json_encode($datos) . PHP_EOL,FILE_APPEND);
+}
+
+switch ($type) {
+    case "payment":
+        $payment = MercadoPago\Payment::find_by_id($id);
+        if (!empty($payment)) {
+            header("HTTP/1.1 200 OK");
+
+        } else {
+            header("HTTP/1.1 400 NOT_OK");
+        }
+        break;
+    case "plan":
+        $plan = MercadoPago\Plan::find_by_id($id);
+        if (!empty($plan)) {
+            header("HTTP/1.1 200 OK");
+        } else {
+            header("HTTP/1.1 400 NOT_OK");
+        }
+        break;
+    case "subscription":
+        $plan = MercadoPago\Subscription::find_by_id($id);
+        if (!empty($plan)) {
+            header("HTTP/1.1 200 OK");
+        } else {
+            header("HTTP/1.1 400 NOT_OK");
+        }
+        break;
+    case "invoice":
+        $plan = MercadoPago\Invoice::find_by_id($id);
+        if (!empty($plan)) {
+            header("HTTP/1.1 200 OK");
+        } else {
+            header("HTTP/1.1 400 NOT_OK");
+        }
+        break;
+    default:
+        header("HTTP/1.1 200 OK");
+        break;
+
+}
