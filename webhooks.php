33dc3ec5c994e264339f55e8e1fa7fbecbff9af6
kevocam.seo@gmail.com <kevocam.seo@gmail.com>
2020-12-30 16:10:21 -0500

    test

--- webhooks.php
+++ webhooks.php
@@ -1,31 +1,13 @@
 <?php
-// Loading Composer
-require __DIR__ . '/vendor/autoload.php';
-DK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
+require_once 'vendor/autoload.php';
 
-MercadoPago\SDK::
+MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
 
-$file = 'logs.txt';
-
-$json_event = file_get_contents( 'php://input', true );
-$event = json_decode( $json_event );
-
-if ( isset( $event->type, $event->data->id ) ) {
-    $event_type = $event->type;
-    $event_id = $event->data->id;
-
-    $payment = MercadoPago\Payment::find_by_id( $event_id );
-
-    $current = file_get_contents( $file );
-    $current .= $event_type . " recibido - ID #" . $payment->id . "\n";
-
-} else {
-    $current = file_get_contents( $file );
-    $current .= "Llamada directa\n";
-}
-
-file_put_contents( $file, $current );
-
-http_response_code( 200 );
+MercadoPago\SDK::setIntegratorId("dev_2e4ad5dd362f11eb809d0242ac130004");
+$filename = 'response.json';
 
 
+$data = file_get_contents('php://input') . PHP_EOL;
+file_put_contents($filename, print_r($data, true), FILE_APPEND);
+
+?>
