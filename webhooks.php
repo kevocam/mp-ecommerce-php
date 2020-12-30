206d5213a03f1a3a8126ed53f89657d4e21aedf0
kevocam.seo@gmail.com <kevocam.seo@gmail.com>
2020-12-29 22:01:22 -0500

    mas

--- webhooks.php
+++ webhooks.php
@@ -1,12 +1,43 @@
 
+
 <?php
+http_response_code(200);
+
+require('vendor/autoload.php');
+
 MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
 
 MercadoPago\SDK::setIntegratorId("dev_2e4ad5dd362f11eb809d0242ac130004");
 
-require __DIR__ .  '/vendor/autoload.php';
 
 
-var_dump($_POST, $_GET);
-$content = json_encode($_POST);
-file_put_contents('log', $content);
+$rawdata = file_get_contents("php://input");
+
+file_put_contents("webhook.json","/---------------------------------------------------\\\n",FILE_APPEND);
+file_put_contents("webhook.json","Llamada:\n",FILE_APPEND);
+file_put_contents("webhook.json","GET:\n",FILE_APPEND);
+file_put_contents("webhook.json",json_encode($_GET) . "\n",FILE_APPEND);
+file_put_contents("webhook.json","POST:\n",FILE_APPEND);
+file_put_contents("webhook.json",json_encode($rawdata) . "\n",FILE_APPEND);
+file_put_contents("webhook.json","---- Resultado ----\n",FILE_APPEND);
+
+if(isset($_GET['topic'])){
+    switch($_GET['topic']){
+        case 'payment':
+            $result = MercadoPago\Payment::find_by_id($_GET['id']);
+            break;
+        case 'merchant_order':
+            $result = MercadoPago\MerchantOrder::find_by_id($_GET['id']);
+            break;
+        default:
+            $result = "Topic no reconocido.";
+        break;
+    }
+} else {
+    $result = "Topic no recibido.";
+}
+
+file_put_contents("webhook.json",json_encode($result) . "\n",FILE_APPEND);
+file_put_contents("webhook.json","\---------------------------------------------------/\n",FILE_APPEND);
+
+?>
